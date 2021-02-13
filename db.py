e2ca75244c68f7554d3b25562497ff37f174e9be
Ryan Ittyipe <ryanitt@gmail.com>
2021-02-13 15:36:43 -0700

    added class def and started table

--- db.py
+++ db.py
@@ -1,28 +1,53 @@
 import mysql.connector
+import datetime
+class DataBase:
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="mainAd",
-  password="K3nnyisjeonmayer",
-  database="jarvisfc"
-)
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+                    host="localhost",
+                    user="mainAd",
+                    password="K3nnyisjeonmayer",
+                    database="jarvisfc"
+                    )
+        self.mycursor = self.mydb.cursor()
+        
+        
+
+    def createMeetingsTable(self):
+        created = False
+        self.mycursor.execute("SHOW TABLES")
+        for x in self.mycursor:
+            if(x[0] == 'meetings'):
+                created = True
+
+        if not created:
+            self.mycursor.execute("CREATE TABLE meetings (\
+                            id INTEGER NOT NULL AUTO_INCREMENT,\
+                            topic VARCHAR(255),\
+                            time DATETIME,\
+                            PRIMARY KEY (id)\
+                            );")
+            print('created meetings')
 
 
-mycursor = mydb.cursor()
-mycursor.execute("SHOW TABLES")
+    def createMeeting(self, topic, Y, M, D, h, m):
+        meeting_date = datetime.datetime(Y, M, D, hour=h, minute=m).strftime('%Y-%m-%d %H:%M:%S')
+        print(meeting_date)
+        sql = "INSERT INTO meetings (topic, time) VALUES (%s, %s)"
+        val = (topic, meeting_date)
+        self.mycursor.execute(sql, val)
 
-created = False
+
+    def showMeetings(self):
+        self.mycursor.execute("SELECT * FROM meetings")
+
+        for x in self.mycursor:
+            print(x)
 
 
-for x in mycursor:
-    if(x[0] == 'meetings'):
-        created = True
 
-if not created:
-    mycursor.execute("CREATE TABLE meetings (topic VARCHAR(255), time DATETIME)")
-    print('created meetings')
-
-mycursor.execute("SHOW TABLES")
-
-for x in mycursor:
-  print(x)
+if __name__ == "__main__":
+    db = DataBase()
+    # db.createMeetingsTable()
+    db.createMeeting("your dad", 1997, 1, 31, 13, 45)
+    db.showMeetings()
