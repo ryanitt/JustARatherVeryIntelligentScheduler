5b8f7a0656aa58fa108793282db9d3a1c4d459c8
Ryan Ittyipe <ryanitt@gmail.com>
2021-02-13 14:42:12 -0700

    created local db with basic create commands

--- db.py
+++ db.py
@@ -2,8 +2,27 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="mainAd",
+  password="K3nnyisjeonmayer",
+  database="jarvisfc"
 )
 
-print(mydb)
+
+mycursor = mydb.cursor()
+mycursor.execute("SHOW TABLES")
+
+created = False
+
+
+for x in mycursor:
+    if(x[0] == 'meetings'):
+        created = True
+
+if not created:
+    mycursor.execute("CREATE TABLE meetings (topic VARCHAR(255), time DATETIME)")
+    print('created meetings')
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
