05d56f243ea8cd1a21780f13577f5f1c524d4fc2
Ryan Ittyipe <ryanitt@gmail.com>
2021-02-13 16:14:59 -0700

    fixed commit status and added persons

--- db.py
+++ db.py
@@ -10,8 +10,10 @@ class DataBase:
                     database="jarvisfc"
                     )
         self.mycursor = self.mydb.cursor()
-        
-        
+
+    def saveToDB(self):
+        self.mycursor.execute("COMMIT;")
+
 
     def createMeetingsTable(self):
         created = False
@@ -29,18 +31,45 @@ class DataBase:
                             );")
             print('created meetings')
 
+    def createPersonsTable(self):
+        created = False
+        self.mycursor.execute("SHOW TABLES")
+        for x in self.mycursor:
+            if(x[0] == 'persons'):
+                created = True
+
+        if not created:
+            self.mycursor.execute("CREATE TABLE persons (\
+                            id INTEGER NOT NULL AUTO_INCREMENT,\
+                            discord VARCHAR(255),\
+                            tag INTEGER,\
+                            PRIMARY KEY (id)\
+                            );")
+            print('created persons')
+
+    def createTables(self):
+        self.createMeetingsTable()
+        self.createPersonsTable()
 
     def createMeeting(self, topic, Y, M, D, h, m):
         meeting_date = datetime.datetime(Y, M, D, hour=h, minute=m).strftime('%Y-%m-%d %H:%M:%S')
         print(meeting_date)
-        sql = "INSERT INTO meetings (topic, time) VALUES (%s, %s)"
+        sql = "INSERT INTO meetings (topic, time) VALUES (%s, %s);"
         val = (topic, meeting_date)
         self.mycursor.execute(sql, val)
+        
+    def createPerson(self, disc, tag):
+        sql = "INSERT INTO persons (discord, tag) VALUES (%s, %s)"
+        val = (disc, tag)
+        print(sql, val)
+        self.mycursor.execute(sql, val)
 
-
-    def showMeetings(self):
+    def showInfo(self):
         self.mycursor.execute("SELECT * FROM meetings")
+        for x in self.mycursor:
+            print(x)
 
+        self.mycursor.execute("SELECT * FROM persons")
         for x in self.mycursor:
             print(x)
 
@@ -48,6 +77,16 @@ class DataBase:
 
 if __name__ == "__main__":
     db = DataBase()
-    # db.createMeetingsTable()
-    db.createMeeting("your dad", 1997, 1, 31, 13, 45)
-    db.showMeetings()
+    db.createTables()
+    db.createMeeting("meeting 1", 1997, 1, 31, 13, 45)
+    db.createMeeting("meeting 2", 1998, 2, 1, 7, 30)
+    db.createMeeting("meeting 3", 1999, 3, 2, 2, 00)
+    db.createMeeting("meeting 4", 2000, 4, 3, 15, 15)
+    db.createPerson("Question", "0874")
+    db.createPerson("Rice", "2405")
+    db.createPerson("FunkyPants4457", "6186")
+    db.createPerson("chendaddy15", "6336")
+    db.createPerson("Kurozinx", "7652")
+
+    db.showInfo()
+    db.saveToDB()
